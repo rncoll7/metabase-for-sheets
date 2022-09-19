eefb895b611bde2635b6a24de130eec87830763b
renan <renan@easyc.com.br>
2022-09-19 01:20:25 -0300

    alet

--- app/src/SidebarController.js
+++ app/src/SidebarController.js
@@ -1,74 +1,114 @@
 function processConfigForm(object) {
-    if (!object.base_url) {
-        return "!base_url"
+    try {
+        if (!object.base_url) {
+            return "!base_url"
+        }
+        if (!object.username) {
+            return "!username"
+        }
+        if (!object.password) {
+            return "!password"
+        }
+    
+        properties.setBaseUrl(object.base_url);
+        properties.setUsername(object.username);
+        properties.setPassword(object.password);
+        return true;
+    } catch (e) {
+        SpreadsheetApp.getUi().alert(e);
+        return false;
     }
-    if (!object.username) {
-        return "!username"
-    }
-    if (!object.password) {
-        return "!password"
-    }
-
-    properties.setBaseUrl(object.base_url);
-    properties.setUsername(object.username);
-    properties.setPassword(object.password);
-    return true
 }
 
 function feachConfigForm() {
-    return {
-        "base_url": properties.getBaseUrl(),
-        "username": properties.getUsername()
+    try { 
+        return {
+            "base_url": properties.getBaseUrl(),
+            "username": properties.getUsername()
+        }
+    } catch (e) {
+        SpreadsheetApp.getUi().alert(e);
+        return false;
     }
 }
 
 function processEditForm(object) {
-    if (!object.query_id) {
-        return "!query_id"
+    try {
+        if (!object.query_id) {
+            return "!query_id"
+        }
+        if (!object.name) {
+            return "!name"
+        }
+        if (!object.sheet) {
+            return "!sheet"
+        }
+    
+        query = {
+            "id": object.query_id,
+            "name": object.name,
+            "sheet": object.sheet,
+            "range": object.range,
+            "parameters": []
+        }
+    
+        const parameters = JSON.parse(object.parameters || '[]');
+        for (let i = 0; i < parameters.length; i++) {
+            let parameter = parameters[i];
+            parameter.value = object[parameter.slug];
+            query.parameters.push(parameter);
+        }
+    
+        properties.setQuery(query);
+        return true;
+    } catch (e) {
+        SpreadsheetApp.getUi().alert(e);
+        return false;
     }
-    if (!object.name) {
-        return "!name"
-    }
-    if (!object.sheet) {
-        return "!sheet"
-    }
-
-    query = {
-        "id": object.query_id,
-        "name": object.name,
-        "sheet": object.sheet,
-        "range": object.range,
-        "parameters": []
-    }
-
-    const parameters = JSON.parse(object.parameters || '[]');
-    for (let i = 0; i < parameters.length; i++) {
-        let parameter = parameters[i];
-        parameter.value = object[parameter.slug];
-        query.parameters.push(parameter);
-    }
-
-    properties.setQuery(query);
-    return {query: query, object: object};
 }
 
 function feachEditForm(id) {
-    return properties.getQuery(id || 7857)
+    try {
+        return properties.getQuery(id || 13);   
+    } catch (e) {
+        SpreadsheetApp.getUi().alert(e);
+        return false;
+    }
 }
 
 function feachQueries() {
-    return properties.getQueries()
+    try {
+        return properties.getQueries();   
+    } catch (e) {
+        SpreadsheetApp.getUi().alert(e);
+        return false;
+    }
 }
 
 function feachQueryDetails(id) {
-    return Core.getQuestionDetailsSimplified(id || 7857);
+    try {
+        return Core.getQuestionDetailsSimplified(id || 13);        
+    } catch (e) {
+        SpreadsheetApp.getUi().alert(e);
+        return false;
+    }
 }
 
 function clean() {
-    properties.setQueries([])
+    try {
+        properties.setQueries([]);        
+    } catch (e) {
+        SpreadsheetApp.getUi().alert(e);
+        return false;
+    }
 }
 
 function executeQuery(id) {
-    let query = properties.getQuery(id || 7857);
-    return Core.getQuestionAndFillSheet(query);
+    try {
+        let query = properties.getQuery(id || 13);
+        return Core.getQuestionAndFillSheet(query);        
+    } catch (e) {
+        SpreadsheetApp.getUi().alert(e);
+        return false;
+    }
 }
